9fe06682fe4da9b8e2d07996f4d09eee5b529303
seonhyeong <752312@naver.com>
2021-03-17 18:02:13 +0900

    Java Persistence API 사용 - JPA 사용을 위한 Dependency 추가와 설정 - Spring Data JPA를 이용한 Entity 설정과 초기 데이터 생성 - JPA Service 구현을 위한 Controller, Repository 생성 - JPA를 이용한 사용자 목록 조회 - GET HTTP Method - JPA를 이용한 사용자 추가와 삭제 - POST/DELETE HTTP Method

--- restful-web-service/src/main/java/com/example/demo/user/User.java
+++ restful-web-service/src/main/java/com/example/demo/user/User.java
@@ -2,7 +2,9 @@ package com.example.demo.user;
 
 import java.util.Date;
 
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -22,7 +24,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor //defaul 생성자
 //@JsonFilter("UserInfo")
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity //JPA에서 DB자동 생성 어노테이션
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="Name은 2글자 이상 입력해 주세요.")
---
+++ restful-web-service/src/main/java/com/example/demo/user/UserJpaController.java
@@ -0,0 +1,58 @@
+package com.example.demo.user;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/jpa")
+public class UserJpaController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	// http://localhost:8088/jpa/users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		Optional<User> user = userRepository.findById(id);
+		
+		if(!user.isPresent()) {
+			throw new UserNotFoundException(String.format("ID[%s] is not found",id));
+		}
+		return user.get();
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userRepository.deleteById(id);
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+		User savedUser = userRepository.save(user);
+		
+		URI location =ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
+}
