43775fc1cf2db9354cf3d776173869b904c6737d
seonhyeong <752312@naver.com>
2021-03-18 14:35:05 +0900

    게시물 관리를 위한 Post Entity 추가와 초기 데이터 생성 게시물 조회를 위한 Post Entity와 User Entity와의 관계 설정 JPA를 이용한 새 게시물 추가 - POST HTTP Method

---
+++ restful-web-service/src/main/java/com/example/demo/user/Post.java
@@ -0,0 +1,34 @@
+package com.example.demo.user;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String description;
+	
+	// User : Post -> 1 : (0~N), Main : Sub -> Parent : Child
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore
+	private User user;
+	
+
+}
--- restful-web-service/src/main/java/com/example/demo/user/User.java
+++ restful-web-service/src/main/java/com/example/demo/user/User.java
@@ -1,10 +1,12 @@
 package com.example.demo.user;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -26,6 +28,14 @@ import lombok.NoArgsConstructor;
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 @Entity //JPA에서 DB자동 생성 어노테이션
 public class User {
+	public User(int id, String name, Date joinDate, String password, String ssn) {
+		this.id = id;
+		this.name = name;
+		this.joinDate = joinDate;
+		this.password = password;
+		this.ssn = ssn;
+	}
+
 	@Id
 	@GeneratedValue
 	private Integer id;
@@ -40,4 +50,10 @@ public class User {
 	private String password;
 	@ApiModelProperty(notes = "사용자의 주민번호를 입력해 주세요.")
 	private String ssn;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Post> posts;
+	
+	
+	
 }
